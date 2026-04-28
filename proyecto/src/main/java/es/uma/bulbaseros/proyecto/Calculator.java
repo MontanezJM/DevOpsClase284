e261eaa629beb96e339948afc30dfee0439fa774
MontanezJM <jmmontanezmora@gmail.com>
2026-04-28 12:07:48 +0200

    nigga

---
+++ proyecto/src/main/java/es/uma/bulbaseros/proyecto/Calculator.java
@@ -0,0 +1,23 @@
+package es.uma.bulbaseros.proyecto;
+
+/**
+ * Basic calculator with arithmetic operations.
+ */
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        return a / b;
+    }
+}
